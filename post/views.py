354d52ca946093c2169befee0b65cefe4aae0c99
AhmedSamy <ahmedmsamy01@gmail.com>
2025-03-30 15:50:05 +0200

    edit view file

--- post/views.py
+++ post/views.py
@@ -67,6 +67,9 @@ class PostDetail(DetailView):
     
     # add comment to the post
     # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['comment_form'] = CommentForm()
+    #     return context
 
     # template_name = 'post/post_detail.html'
     # context_object_name = 'post' ## in template [post]
